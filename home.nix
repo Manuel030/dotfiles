08c7d542c5911a5e7d824609d0de7df83a971a6a
Manuel030 <manuelenrique.plank@gmail.com>
2024-11-22 13:19:04 +0100

    add gitlab activity script

--- home.nix
+++ home.nix
@@ -36,6 +36,11 @@ in
       enableCompletion = false;
       bashrcExtra = ''
         export PATH="$HOME/.rye/shims:$PATH"
+
+        set -a
+        if [ -f "$HOME/Projects/dotfiles/.secrets" ]; then
+            source $HOME/Projects/dotfiles/.secrets
+        fi;
       '';
     };
     fzf = {
@@ -100,6 +105,7 @@ in
     bat
     dive
     jless
+    jq
     nixpkgs-fmt
     tree
     btop
@@ -118,6 +124,60 @@ in
     else throw "Unknown OS")
   ;
 
+  home.file = {
+    ".local/bin/gitlab-activity" = {
+      executable = true;
+      text = ''
+        #!${pkgs.bash}/bin/bash
+        if [ -z "$GITLAB_TOKEN" ]; 
+        then
+          echo "No GITLAB_TOKEN provided"
+          exit 1;
+        fi;
+        read -p "Enter date difference to today: " DAYS
+        AFTER_DATE=$(date -v-"$(($DAYS+1))"d +%Y-%m-%d)
+        if [ $DAYS == 1 ]
+        then
+          BEFORE_DATE=$(date +%Y-%m-%d);
+        elif [ $DAYS == 0 ]
+        then
+          BEFORE_DATE=$(date -v+1d +%Y-%m-%d);
+        else
+          BEFORE_DATE=$(date -v-"$(($DAYS-1))"d +%Y-%m-%d);
+        fi;
+        echo "Querying activity after $AFTER_DATE and before $BEFORE_DATE"
+        GITLAB_USER="manuelpland"
+        COMMIT_LOG=$(curl -s --header "PRIVATE-TOKEN: $GITLAB_TOKEN" "https://gitlab.com/api/v4/users/$GITLAB_USER/events?after=$AFTER_DATE&before=$BEFORE_DATE&per_page=100" \
+          | jq '.[] | { action: .action_name, description: (if .target_title != null then .target_title elif .push_data != null then .push_data.commit_title elif .note != null then .note.body else "No title" end), branch: (if .action_name == "pushed to" then .push_data.ref else "no code change" end)}')
+
+        echo "Your commit log"
+        echo $COMMIT_LOG | jq
+
+        PROMPT="Describe the programmers work based on the commit log in a concise manner. Only do the task without any introductions and write from the programmers perspective. Dont do any formatting. $COMMIT_LOG"
+
+        PAYLOAD=$(jq -n \
+          --arg model "llama3-8b-8192" \
+          --arg content "$PROMPT" \
+          '{
+            model: $model,
+            messages: [
+              {
+                role: "user",
+                content: $content
+              }
+            ]
+          }'
+        )
+
+        echo "Your summary"
+        curl https://api.groq.com/openai/v1/chat/completions -s \
+          -H "Content-Type: application/json" \
+          -H "Authorization: Bearer $GROQ_API_KEY" \
+          -d "$PAYLOAD" | jq .choices[0].message.content
+      '';
+    };
+  };
+
   home.shellAliases = {
     "ll" = "ls -Fahl";
     "," = ''f() { nix run nixpkgs#"$1" -- "{@:2}" ; }; f'';
@@ -126,5 +186,6 @@ in
     "home" = ''home-manager switch --flake ~/Projects/dotfiles/'';
     "dcu" = ''docker compose up'';
     "dcd" = ''docker compose down'';
+    "activity" = "~/.local/bin/gitlab-activity";
   };
 }
